49b3ea64ac8e6acfb1d6f78c0cc6d84aafe51090
Pedro Dalben <pedrodalbenmorais@gmail.com>
2025-08-16 03:52:33 -0300

    fix: corrige posicionamento do header para mobile e desktop
    - Remove calc() problemático que não funcionava em mobile
    - Implementa lógica responsiva: mobile (largura total) vs desktop (respeita sidebar)
    - Adiciona listener para redimensionamento da janela
    - Header agora funciona corretamente em todos os dispositivos
    - Limpa event listeners no disconnect

--- app/frontend/javascript/controllers/header_component_controller.js
+++ app/frontend/javascript/controllers/header_component_controller.js
@@ -10,10 +10,17 @@ export default class extends Controller {
     console.log("Header component controller connected")
     this.darkModeValue = JSON.parse(localStorage.getItem('darkMode') || 'false')
     this.updatePosition()
+    
+    // Listener para redimensionamento da janela
+    this.resizeHandler = this.updatePosition.bind(this)
+    window.addEventListener('resize', this.resizeHandler)
   }
 
   disconnect() {
     localStorage.setItem('darkMode', JSON.stringify(this.darkModeValue))
+    if (this.resizeHandler) {
+      window.removeEventListener('resize', this.resizeHandler)
+    }
   }
 
   toggleSidebar() {
@@ -31,7 +38,16 @@ export default class extends Controller {
     if (sidebar) {
       const isCollapsed = sidebar.classList.contains('xl:w-[90px]')
       const leftPosition = isCollapsed ? '90px' : `${this.sidebarWidthValue}px`
-      this.element.style.left = `calc(100vw - ${leftPosition})`
+      
+      // Em mobile, header ocupa toda largura
+      if (window.innerWidth < 1280) {
+        this.element.style.left = '0'
+        this.element.style.right = '0'
+      } else {
+        // Em desktop, header respeita sidebar
+        this.element.style.left = leftPosition
+        this.element.style.right = '0'
+      }
     }
   }
 }
