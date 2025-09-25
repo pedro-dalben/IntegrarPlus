acf5fd79f853cc47d64253458fa5e83e0ff76bac
Pedro Dalben <pedrodalbenmorais@gmail.com>
2025-09-25 16:53:48 -0300

    Fix: Resolve migration and ApplicationCable issues
    - Fixed migration to check column existence before renaming
    - Added ApplicationCable base classes to resolve loading issues
    - Migration now safely handles visibility_scope to visibility_level rename

---
+++ app/channels/application_cable.rb
@@ -0,0 +1,7 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+  end
+
+  class Channel < ActionCable::Channel::Base
+  end
+end
--- db/migrate/20250903214149_rename_visibility_scope_to_visibility_level.rb
+++ db/migrate/20250903214149_rename_visibility_scope_to_visibility_level.rb
@@ -1,5 +1,13 @@
 class RenameVisibilityScopeToVisibilityLevel < ActiveRecord::Migration[8.0]
-  def change
-    rename_column :events, :visibility_scope, :visibility_level
+  def up
+    if column_exists?(:events, :visibility_scope)
+      rename_column :events, :visibility_scope, :visibility_level
+    end
+  end
+
+  def down
+    if column_exists?(:events, :visibility_level)
+      rename_column :events, :visibility_level, :visibility_scope
+    end
   end
 end
