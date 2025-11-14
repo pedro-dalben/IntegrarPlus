91999187a03a784e8496d4349375107bfa5b5f64
Pedro Dalben <pedrodalbenmorais@gmail.com>
2025-11-14 16:40:46 -0300

    feat: adiciona redirect para usuários autenticados na tela de login
    - Evita que usuários já autenticados acessem a página de login
    - Redireciona para a página inicial após autenticação

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,10 +2,22 @@
 
 module Users
   class SessionsController < Devise::SessionsController
+    prepend_before_action :redirect_authenticated_user, only: [:new]
     after_action :set_user_last_seen, only: [:create]
     after_action :clear_user_last_seen, only: [:destroy]
 
     private
+
+    def redirect_authenticated_user
+      return unless warden.authenticated?(resource_name)
+
+      respond_to do |format|
+        format.html do
+          redirect_to(after_sign_in_path_for(current_user || warden.user(resource_name)))
+        end
+        format.any { head :ok }
+      end
+    end
 
     def set_user_last_seen
       return unless user_signed_in?
